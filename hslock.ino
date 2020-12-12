6d23681351f43d25efdce844fc2e0f56165839a5
cb4b1fd915 <>
2020-12-12 18:08:58 +0000

    Remove unnecessary spaces in hslock.ino file

--- hslock.ino
+++ hslock.ino
@@ -4,48 +4,48 @@
 #include <Wire.h>
 #include <TroykaRTC.h>
 #include "Secrets.h"
- 
+
 
 #define BUZZER_PIN 9
 #define LOCK_PIN 12
 #define BUTTON_OPEN_PIN 10
- 
+
 #define SOUND_TIME_BUTTON_PRESS 50
 #define SOUND_TIME_OPEN 3000
 #define SOUND_TIME_ERROR 750
 
 #define FREQ_BUTTON_PRESS 5000
 #define FREQ_OPEN 2500
-#define FREQ_ERROR 800   
- 
+#define FREQ_ERROR 800
+
 
 // Keypad
-const byte ROWS = 4;  
-const byte COLS = 3;   
+const byte ROWS = 4;
+const byte COLS = 3;
 const char hexaKeys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
   {'*','0','#'}
 };
-const byte rowPins[ROWS] = {2, 3, 4, 5};  
-const byte colPins[COLS] = {6, 7, 8};  
+const byte rowPins[ROWS] = {2, 3, 4, 5};
+const byte colPins[COLS] = {6, 7, 8};
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
-// RTC 
+// RTC
 RTC clock;
 
-String userInput = "";  
+String userInput = "";
 
 
 void setup(){
   Serial.begin(9600);
-  pinMode(BUTTON_OPEN_PIN, INPUT); 
-  pinMode(BUZZER_PIN, OUTPUT); 
+  pinMode(BUTTON_OPEN_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   pinMode(LOCK_PIN, OUTPUT);
-  
-  clock.begin(); 
-  clock.set(__TIMESTAMP__); 
+
+  clock.begin();
+  clock.set(__TIMESTAMP__);
 }
 
 
@@ -55,23 +55,23 @@ void accessDenied() {
 }
 
 
-void unlockTheDoor() {  
-  digitalWrite(LOCK_PIN, HIGH); 
-  tone(BUZZER_PIN, FREQ_OPEN, SOUND_TIME_OPEN); 
+void unlockTheDoor() {
+  digitalWrite(LOCK_PIN, HIGH);
+  tone(BUZZER_PIN, FREQ_OPEN, SOUND_TIME_OPEN);
   delay(SOUND_TIME_OPEN);
   digitalWrite(LOCK_PIN, LOW);
 } 
 
 
-boolean arrayIncludeElement(int data[], int arraySize,  int element) {  
-  for (int i = 0; i < arraySize; i++) {  
+boolean arrayIncludeElement(int data[], int arraySize,  int element) {
+  for (int i = 0; i < arraySize; i++) {
     if (data[i] == element) {
       return true;
     }
-  } 
+  }
   return false;
 }
-  
+
 
 bool isTOTPCodeValid(String userInput) { 
   Serial.print("userInput: "); Serial.println(userInput);
@@ -82,57 +82,58 @@ bool isTOTPCodeValid(String userInput) {
   if (!keyIsActive) {
     accessDenied();
     return false;
-  };  
-  clock.read();   
-  delay(200); 
-  clock.read(); 
-  unsigned long currentUnixTimestamp = clock.getUnixTime() + 86400 - 3600; 
-  int timeDeviations[3] = {-30, 0, 30};  
+  };
+  clock.read();
+  delay(200);
+  clock.read();
+  unsigned long currentUnixTimestamp = clock.getUnixTime() + 86400 - 3600;
+  int timeDeviations[3] = {-30, 0, 30};
   String userCode = userInput.substring(2, 8);
   Serial.print("Time: ");
-  Serial.println(currentUnixTimestamp);  
+  Serial.println(currentUnixTimestamp);
   TOTP totp = TOTP(secrets::hmacKeys[keyNum], secrets::hmacKeySize);
   for (int i = 0; i < 3; i++) {
-    int delta = timeDeviations[i]; 
-    char* correctCode = totp.getCode(currentUnixTimestamp + delta); 
+    int delta = timeDeviations[i];
+    char* correctCode = totp.getCode(currentUnixTimestamp + delta);
     Serial.println(userCode);
-    Serial.println(correctCode); 
+    Serial.println(correctCode);
     if (userCode == correctCode) {
       Serial.println("+++++++++++++++++++++++");
       return userCode == correctCode;
     }
-  } 
+  }
   Serial.println("==========================");
   accessDenied();
   return false;
 }
 
-  
+
 void loop(){ 
-  digitalWrite(LOCK_PIN, LOW);   
+  digitalWrite(LOCK_PIN, LOW);
+
   boolean openButtonIsDown = digitalRead(BUTTON_OPEN_PIN);
- 
-  if (openButtonIsDown) { 
+  if (openButtonIsDown) {
     unlockTheDoor();
   };
-  
-  char customKey = customKeypad.getKey();  
-  if (customKey){     
+
+  char customKey = customKeypad.getKey();
+  if (customKey){
     tone(BUZZER_PIN, FREQ_BUTTON_PRESS, SOUND_TIME_BUTTON_PRESS);
-    if (customKey == '*') {  
+    if (customKey == '*') {
       userInput = "";
     }
-    else if (customKey == '#') {    
+    else if (customKey == '#') {
       if (isTOTPCodeValid(userInput)) {
         unlockTheDoor();
       }; 
       userInput = "";
     } else {
-      if (userInput.length() == 8) {  
+      if (userInput.length() == 8) {
         userInput = "";
       };
-      userInput += customKey;   
+      userInput += customKey;
       delay(50);
-    }; 
+    };
   }
 }
+
